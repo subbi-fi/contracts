8d4b4634a96d8b9f33e5cf63587dabebf6c58ffd
Aaron <aarongkendall@gmail.com>
2022-03-21 11:56:42 +0000

    update deployment to configure the correct billing cycle for production

--- migrations/2_subscription_deployments.js
+++ migrations/2_subscription_deployments.js
@@ -7,7 +7,8 @@ dotenv.config();
 const usdcAddress = process.env.USDC_ADDRESS;
 const signer = process.env.SIGNER_ADDRESS;
 const ONE_USDC = 1000000;
-const ONE_DAY_IN_SECONDS = 60 * 60 * 24 * 1;
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+const THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30;
 
 module.exports = (deployer, network) => {
   if (network === "development") return;
@@ -15,6 +16,7 @@ module.exports = (deployer, network) => {
     await deployer.deploy(SubscriptionConfig, 30, usdcAddress, signer);
     const configInstance = await SubscriptionConfig.deployed();
 
-    await deployer.deploy(SubscriptionFactory, configInstance.address, ONE_USDC, ONE_DAY_IN_SECONDS);
+    const billingInterval = network === "polygontestnet" ? ONE_DAY_IN_SECONDS : THIRTY_DAYS_IN_SECONDS;
+    await deployer.deploy(SubscriptionFactory, configInstance.address, ONE_USDC, billingInterval);
   })
 };
